04627d3e9aa9d112dac09392b286971c3d78c1d8
Colin <cstatro@gmail.com>
2019-09-08 01:55:14 -0400

    route is working

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,3 +1,3 @@
 class GameSerializer < ActiveModel::Serializer
-  attributes :id,:name,:max_player,:min_player,:max_playtime,:min_playtime,:image,:thumbnail
+  attributes :id,:name,:max_player,:min_player,:max_playtime,:min_playtime,:image,:thumbnail,:users
 end
